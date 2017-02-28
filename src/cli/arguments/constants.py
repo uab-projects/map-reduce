2d6896c3abd8caa42639184c9fa912d0f069f574
David LJ <mail@davidlj95.com>
2017-02-28 14:08:16 +0100

    Added main workflow

--- src/cli/arguments/constants.py
+++ src/cli/arguments/constants.py
@@ -1,8 +1,14 @@
-
-#Log level
+# Log level
 """
 Default log level
 """
-LOGS = ["debug","info","warning","error","critical"]
-LOGS_LEVELS = [10,20,30,40,50]
+LOGS = ["debug", "info", "warning", "error", "critical"]
+LOGS_LEVELS = [10, 20, 30, 40, 50]
 LOG_DEFAULT = LOGS[1]
+
+# Tasks for map - reduce
+"""
+Sets the task to perform by the map - reduce paradigm
+"""
+TASKS = ["word-count", "letter-count"]
+TASK_DEFAULT = TASKS[0]
--- src/core/log/__init__.py
+++ src/core/log/__init__.py
@@ -1,9 +1,20 @@
+"""
+Allows to enable the Python logging system
+"""
 # Load default configuration
 import logging
 import logging.config
 
 # Constants
 CONFIG_FILE = "config/logging.conf"
+"""
+    str: default configuration file for logging
+"""
+
 
 # Load config
-logging.config.fileConfig(CONFIG_FILE)
+def init():
+    """
+    Initializes the logger with the default configuration
+    """
+    logging.config.fileConfig(CONFIG_FILE)
